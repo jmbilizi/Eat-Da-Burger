9b4e594f9d24b6d96c7948c40ab7a0c8e5734132
Admin <admin@Admins-MacBook-Pro.local>
2020-04-07 11:25:37 -0600

    changed the config info

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,13 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Janvier1995@",
-  database: "burgersDB"
+  host: "s0znzigqvfehvff5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  user: "eydxu0ukl9d49vlw",
+  password: "m3jipn1qzdtkvgw4",
+  database: "nm3amn9ex05769it",
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
